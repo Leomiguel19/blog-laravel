75ca9746f4bfccf84d035b01006fa6b065a1c2d0
Leomiguel19 <leomiguel1907@gmail.com>
2021-09-27 00:22:57 -0400

    Solucionando errores en el diseño

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,11 +3,11 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
                 <article class="w-full h-80 bg-cover bg-center @if($loop->first) md:col-span-2 @endif " style="background-image: url({{Storage::url($post->image->url)}})">
-                    <div class="w-full h-full p-x-8 flex flex-col justify-center">
+                    <div class="w-full h-full px-8 flex flex-col justify-center">
                         <div>
                             @foreach ($post->tags as $tag)                                
-                                <a href="" class="inline-block px-3 h6 bg-{{$tag->color}} text-white rounded-full">
-                                    {{$tag}}
+                                <a href="" class="inline-block px-3 h6 bg-{{$tag->color}}-600 text-white rounded-full">
+                                    {{$tag->name}}
                                 </a>    
                             @endforeach
                         </div>

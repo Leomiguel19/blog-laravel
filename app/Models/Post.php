38837335e615470b0fe0325bcdf73f5324f7e49b
Leomiguel19 <leomiguel1907@gmail.com>
2021-10-02 23:49:09 -0400

    Cambios para crear un nuevo post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,23 +9,25 @@ class Post extends Model
 {
     use HasFactory;
 
-        // Relación uno a muchos inversa
-        public function user(){
-            return $this->belongsTo(User::class);
-        }
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    // Relación uno a muchos inversa
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 
 
-        public function category(){
-            return $this->belongsTo(Category::class);
-        }
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 
-        // Relación muchos a muchos
-        public function tags(){
-            return $this->belongsToMany(Tag::class);
-        }  
-        
-        // Relación uno a uno polimorfica
-        public function image(){
-            return $this->morphOne(Image::class, 'imageable');
-        }
+    // Relación muchos a muchos
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }  
+    
+    // Relación uno a uno polimorfica
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -11,6 +11,8 @@
         <div class="card-body">
             {!! Form::open(['route' => 'admin.posts.store', 'autocomplete' => 'off']) !!}
 
+                {!! Form::hidden('user_id', auth()->user()->id) !!}
+
                 <div class="form-group">
                     {!! Form::label('name', 'Nombre') !!}
                     {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del Post']) !!}
@@ -33,7 +35,7 @@
                     {!! Form::label('category_id', 'Categoría') !!}
                     {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
                 
-                    @error('color')
+                    @error('category_id')
                         <span class="text-danger">{{$message}}</span>
                     @enderror
                 </div>
@@ -47,6 +49,12 @@
                             {{ $tag->name }}
                         </label>                       
                     @endforeach
+
+
+                    @error('tags')
+                        <br>
+                        <span class="text-danger">{{$message}}</span>
+                    @enderror
                 </div>
 
                 <div class="form-group">
@@ -60,16 +68,29 @@
                         {!! Form::radio('status', 2) !!}
                         Publicado
                     </label>
+
+                    @error('status')
+                        <br>
+                        <span class="text-danger">{{$message}}</span>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     {!! Form::label('extract', 'Extracto') !!}
                     {!! Form::textarea('extract', null, ['class' => 'form-control']) !!}
+
+                    @error('extract')
+                        <span class="text-danger">{{$message}}</span>
+                    @enderror
                 </div>
 
                 <div class="form-group">
                     {!! Form::label('body', 'Cuerpo del Post') !!}
                     {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+
+                    @error('body')
+                        <span class="text-danger">{{$message}}</span>
+                    @enderror
                 </div>
 
                 {!! Form::submit('Crear Post', ['class' => 'btn btn-primary']) !!}

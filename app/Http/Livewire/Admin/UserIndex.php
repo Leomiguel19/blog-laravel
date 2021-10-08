c70ce5d3ed4152b14ebd30cee740fe25a223a216
Leomiguel19 <leomiguel1907@gmail.com>
2021-10-07 22:59:43 -0400

    Cambios para asignar roles a usuarios

--- app/Http/Livewire/Admin/UserIndex.php
+++ app/Http/Livewire/Admin/UserIndex.php
@@ -11,13 +11,20 @@ class UserIndex extends Component
 {
 
     use WithPagination;
+
+    public $search;
     
     protected $paginationTheme = "bootstrap";
 
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $users = User::paginate();
-
+        $users = User::where('name', 'LIKE', '%'. $this->search . '%')
+                    ->orWhere('email', 'LIKE', '%'. $this->search . '%')
+                    ->paginate(10);
         return view('livewire.admin.user-index', compact('users'));
     }
 }

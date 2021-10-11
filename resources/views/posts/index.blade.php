a6d5a0a3ede85a7970b6f97b506e422789d9c788
Leomiguel19 <leomiguel1907@gmail.com>
2021-10-10 23:45:25 -0400

    Cache en Laravel

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,8 +20,8 @@
                 </article>
             @endforeach
         </div>
-    </div>
-    <div class="mt-4">
-        {{$posts->links()}}
+        <div class="mt-4">
+            {{$posts->links()}}
+        </div>
     </div>
 </x-app-layout>
